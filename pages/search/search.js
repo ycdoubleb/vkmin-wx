9829ca5be23a7a72b58c5a8c2c5809e640027974
unknown <unknown@example.com>
2019-08-01 17:21:52 +0800

    新增
    新增搜索页

---
+++ pages/search/search.js
@@ -0,0 +1,55 @@
+// pages/search/search.js
+import http from '@chunpu/http';
+import util from '../../utils/util.js';
+import Api from '../../utils/api.js';
+//获取应用实例
+const app = getApp();
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    ready: false,
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    Object.assign(this.data, options);
+    this.ready();
+  },
+
+  /**
+   * 卸载页面
+   */
+  onUnload: function () {
+    // 删除登录事件侦听
+  },
+  //--------------------------------------------------------------------------------------
+  //
+  // 自定义事件
+  //
+  //--------------------------------------------------------------------------------------
+  //--------------------------------------------------------------------------------------
+  //
+  // 自定函数
+  //
+  //--------------------------------------------------------------------------------------
+  /**
+   * 
+   */
+  ready() {
+    // 加载课程详情
+    ready: true
+    // Api.get(Api.GET_TOPIC_DETAIL, { topic_id: this.data.id }).then(data => {
+    //   this.setData({
+    //     ready: true,
+    //     topic: data.topic,
+    //     topicCourses: data.courses
+    //   });
+    // });
+  }
+})
